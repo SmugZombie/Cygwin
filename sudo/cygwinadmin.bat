f7eb52982f6b70256fadfb86c51d8ed6f859ae2e
Ron Egli <ron@r-egli.com>
2015-12-03 17:47:09 -0700

    Create cygwinadmin.bat

---
+++ sudo/cygwinadmin.bat
@@ -0,0 +1,31 @@
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    ::echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+# This is where you set the proper location to your cygwin installation, as mine is located in A, we change the drive to A, then access the appropriate files.
+
+@A:\
+@A:\bin\mintty.exe -i /Cygwin-Terminal.ico -
