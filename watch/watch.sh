959701632218f8e218ea1849114edcd561fb76c9
Ron Egli <ron@r-egli.com>
2015-12-07 14:32:35 -0700

    auto-1449523953

--- watch/watch.sh
+++ watch/watch.sh
@@ -2,10 +2,16 @@
 # A simple watch script for Cygwin as for some reason mine doesnt work.
 # github.com/smugzombie
 
-arguments=$(for i in "$@"; do echo -n "$i "; done)
+arguements=$(for i in "$@"; do echo -n "$i "; done)
+
+if [ -z "$arguements" ]; then
+	echo "Missing Command";
+	exit
+fi
+
 
 while :
 do
-        echo $($arguments)
+        echo $($arguements)
         sleep 2
 done
